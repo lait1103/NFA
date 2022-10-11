54031938419be25c5c12e4d018b61346766c90b3
lait1103 <lait1103@mail.ru>
2022-10-11 07:33:18 +0300

    Implement MinDfa

--- src/main/kotlin/NFA2DFA.kt
+++ src/main/kotlin/NFA2DFA.kt
@@ -4,7 +4,7 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.util.*
 
-data class Input(
+data class InputForNfa(
     val n: Int,
     val m: Int,
     val start: Set<Int>,
@@ -13,7 +13,25 @@ data class Input(
     val inputString: String
 )
 
-fun getInput(`in`: InputStream = System.`in`): Input {
+data class InputForDfa(
+    val n: Int,
+    val m: Int,
+    val start: Int,
+    val finish: Set<Int>,
+    val edges: Map<Pair<Int, Int>, Int>,
+    val inputString: String
+)
+
+fun convertInputNfaToInputDfa(input: InputForNfa): InputForDfa = InputForDfa(
+    input.n,
+    input.m,
+    input.start.first(),
+    input.finish,
+    input.edges.map { (key, value) -> key to value.first() }.toMap(),
+    input.inputString
+)
+
+fun getInputNfa(`in`: InputStream = System.`in`): InputForNfa {
     val scanner = Scanner(`in`)
 
     val n = scanner.nextInt()
@@ -27,13 +45,34 @@ fun getInput(`in`: InputStream = System.`in`): Input {
     while (true) {
         val nextLine = scanner.nextLine()
         if (nextLine.isNullOrEmpty()) break
-        if (!nextLine.contains(" ")) return Input(n, m, start, finish, edges, nextLine)
+        if (!nextLine.contains(" ")) return InputForNfa(n, m, start, finish, edges, nextLine)
         val numbers = nextLine.split(" ").map { it.toInt() }
         if (edges.contains(Pair(numbers[0], numbers[1]))) edges[Pair(numbers[0], numbers[1])]!!.add(numbers[2])
         else edges[Pair(numbers[0], numbers[1])] = mutableListOf(numbers[2])
     }
 
-    return Input(n, m, start, finish, edges, "")
+    return InputForNfa(n, m, start, finish, edges, "")
+}
+
+fun getInputDfa(`in`: InputStream = System.`in`): InputForDfa {
+    val scanner = Scanner(`in`)
+
+    val n = scanner.nextInt()
+    val m = scanner.nextInt()
+    val start = scanner.nextInt()
+    scanner.nextLine()
+    val finishLine = scanner.nextLine()
+    val finish = if (finishLine.isNullOrEmpty()) setOf() else finishLine.split(" ").map { it.toInt() }.toSet()
+    val edges = mutableMapOf<Pair<Int, Int>, Int>()
+    while (true) {
+        val nextLine = scanner.nextLine()
+        if (nextLine.isNullOrEmpty()) break
+        if (!nextLine.contains(" ")) return InputForDfa(n, m, start, finish, edges, nextLine)
+        val numbers = nextLine.split(" ").map { it.toInt() }
+        edges[Pair(numbers[0], numbers[1])] = numbers[2]
+    }
+
+    return InputForDfa(n, m, start, finish, edges, "")
 }
 
 fun getNextReachable(edges: Map<Pair<Int, Int>, List<Int>>, curStep: Set<Int>, a: Int): Set<Int> {
@@ -47,7 +86,7 @@ fun getNextReachable(edges: Map<Pair<Int, Int>, List<Int>>, curStep: Set<Int>, a
 }
 
 fun emulateNFA(`in`: InputStream = System.`in`): Boolean {
-    val input = getInput(`in`)
+    val input = getInputNfa(`in`)
     var nexStep = input.start
     input.inputString.forEach {
         nexStep = getNextReachable(input.edges, nexStep, Character.getNumericValue(it))
@@ -55,22 +94,23 @@ fun emulateNFA(`in`: InputStream = System.`in`): Boolean {
     return nexStep.intersect(input.finish).isNotEmpty()
 }
 
-fun writeOutPut(data: Input, out: OutputStream = System.out) {
+fun writeOutPut(data: InputForDfa, out: OutputStream = System.out, writeInputString: Boolean = false) {
     out.write("${data.n}\n".toByteArray())
     out.write("${data.m}\n".toByteArray())
-    out.write(("${data.start.first()}\n".toByteArray()))
+    out.write(("${data.start}\n".toByteArray()))
     data.finish.forEachIndexed { index, value ->
         if (index != data.finish.size - 1) out.write("$value ".toByteArray()) else out.write(
             "$value".toByteArray()
         )
     }
     out.write("\n".toByteArray())
-    data.edges.forEach { out.write(("${it.key.first} ${it.key.second} ${it.value.first()}\n".toByteArray())) }
-    out.write("\n".toByteArray())
+    data.edges.forEach { out.write(("${it.key.first} ${it.key.second} ${it.value}\n".toByteArray())) }
+    if (writeInputString) out.write((data.inputString + "\n").toByteArray())
+    else out.write("\n".toByteArray())
 }
 
-fun convertNFA2DFA(`in`: InputStream = System.`in`): Input {
-    val input = getInput(`in`)
+fun convertNFA2DFA(`in`: InputStream = System.`in`): InputForNfa {
+    val input = getInputNfa(`in`)
 
     val subsetToIndex = mutableMapOf<Set<Int>, Int>()
     var lastIndex = 0
@@ -99,13 +139,106 @@ fun convertNFA2DFA(`in`: InputStream = System.`in`): Input {
         }
     }
 
-    return Input(
+    return InputForNfa(
         lastIndex,
         input.m,
         setOf(0),
         subsetToIndex.keys.filter { it.intersect(input.finish).isNotEmpty() }.map { subsetToIndex[it]!! }.toSet(),
         answer,
         ""
+    )
+}
+
+fun deleteNotReachable(input: InputForDfa): InputForDfa {
+    val possibleToReach = MutableList(input.n) { false }
+    val queue: Queue<Int> = LinkedList()
+    /// trying to reach all reachable q from q_0
+    possibleToReach[input.start] = true; queue.add(input.start)
+    while (queue.isNotEmpty()) {
+        val elem = queue.first()
+        queue.remove()
+        val edgesFromElem = input.edges.filter { (key, _) -> key.first == elem }.values
+        edgesFromElem.forEach { q ->
+            if (!possibleToReach[q]) {
+                possibleToReach[q] = true; queue.add(q)
+            }
+        }
+    }
+
+    /// recalculate q indexes
+    val oldIndexToNew = mutableMapOf<Int, Int>()
+    var currentIndex = 0
+    possibleToReach.forEachIndexed { index, value ->
+        if (value) oldIndexToNew[index] = currentIndex++
+        else oldIndexToNew[index] = -1
+    }
+
+    /// recalculate indexes in edges
+    val newEdges =
+        input.edges.filter { (key, value) -> oldIndexToNew[key.first]!! != -1 && oldIndexToNew[value]!! != -1 }
+            .map { (key, value) -> (oldIndexToNew[key.first]!! to key.second) to oldIndexToNew[value]!! }.toMap()
+
+    return InputForDfa(
+        currentIndex,
+        input.m,
+        oldIndexToNew[input.start]!!,
+        input.finish.filter { q -> oldIndexToNew[q]!! != -1 }.map { q -> oldIndexToNew[q]!! }.toSet(),
+        newEdges,
+        input.inputString
+    )
+}
+
+fun minDFA(`in`: InputStream = System.`in`): InputForDfa {
+    val input = deleteNotReachable(getInputDfa(`in`))
+
+    var currenAmountOfClasses = 1
+    val classes = mutableMapOf<Int, Int>()
+    (0 until input.n).forEach { classes[it] = if (input.finish.contains(it)) 0 else 1 }
+
+    var wasChanges = true
+    while (wasChanges) {
+        wasChanges = false
+        (0 until currenAmountOfClasses).forEach { classIndex -> // all classes
+            if (!wasChanges) {
+                (0 until input.m).forEach { a -> // all symbols
+                    if (!wasChanges) {
+                        val newClasses = mutableMapOf<Int, MutableList<Int>>()
+                        classes.filter { (_, value) -> value == classIndex }.keys.forEach { q -> //all q
+                            if(input.edges.contains(q to a)) {
+                                val to = input.edges[q to a]!!
+                                if (newClasses.contains(to)) newClasses[to]!!.add(q)
+                                else newClasses[to] = mutableListOf(q)
+                            }
+                        }
+                        if (newClasses.size > 1) {
+                            wasChanges = true
+                            val newClassesList = newClasses.toList()
+                            newClassesList.first().second.forEach { q ->
+                                classes[q] = classIndex
+                            }
+
+                            newClassesList.drop(1).forEach { (_, qs) ->
+                                currenAmountOfClasses++
+                                qs.forEach { q ->
+                                    classes[q] = currenAmountOfClasses
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    val newEdges = input.edges.map { (key, value) -> (classes[key.first]!! to key.second) to classes[value]!! }.toMap()
+
+    return InputForDfa(
+        currenAmountOfClasses + 1,
+        input.m,
+        classes[input.start]!!,
+        input.finish.map { classes[it]!! }.toSet(),
+        newEdges,
+        input.inputString
     )
 }
 
--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -40,16 +40,62 @@ class Tests {
         // Example from the task. I checked that it is done correctly (compared with notes from the class)
         @Test
         fun testConvertor1() {
-            writeOutPut(convertNFA2DFA(File("src/test/kotlin/resources/converter1.txt").inputStream()))
+            writeOutPut(convertInputNfaToInputDfa(convertNFA2DFA(File("src/test/kotlin/resources/converter1.txt").inputStream())))
         }
 
         // DFA (equal to the input)
 
         @Test
         fun testConvertor2() {
-            writeOutPut(convertNFA2DFA(File("src/test/kotlin/resources/converter2.txt").inputStream()), File("testConvertor2").outputStream())
+            writeOutPut(convertInputNfaToInputDfa(convertNFA2DFA(File("src/test/kotlin/resources/converter2.txt").inputStream())), File("testConvertor2").outputStream())
             assertEquals(Files.readAllLines(File("src/test/kotlin/resources/converter2.txt").toPath()),Files.readAllLines(File("testConvertor2").toPath()))
         }
+    }
+
+    class MinDfaTest {
+        @Test
+        fun testMin1() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/converter2.txt").inputStream()), File("testMin1").outputStream())
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/converter2.txt").inputStream()) ,emulateNFA(File("testMin1").inputStream()))
+        }
+
+        @Test
+        fun testMin2() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/emulator2.txt").inputStream()), File("testMin2").outputStream(), true)
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/emulator2.txt").inputStream()) ,emulateNFA(File("testMin2").inputStream()))
+        }
+
+        @Test
+        fun testMin3() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/emulator3.txt").inputStream()), File("testMin3").outputStream(), true)
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/emulator3.txt").inputStream()) ,emulateNFA(File("testMin3").inputStream()))
+        }
+
+        @Test
+        fun testMin4() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/emulator4.txt").inputStream()), File("testMin4").outputStream(), true)
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/emulator4.txt").inputStream()) ,emulateNFA(File("testMin4").inputStream()))
+        }
+
+        @Test
+        fun testMin5() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/emulator5.txt").inputStream()), File("testMin5").outputStream(), true)
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/emulator5.txt").inputStream()) ,emulateNFA(File("testMin5").inputStream()))
+        }
+
+        @Test
+        fun testMin6() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/badDfa1").inputStream()), File("testMin6").outputStream(), true)
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/badDfa1").inputStream()) ,emulateNFA(File("testMin6").inputStream()))
+        }
+
+        @Test
+        fun testMin7() {
+            writeOutPut(minDFA(File("src/test/kotlin/resources/badDfa2").inputStream()), File("testMin7").outputStream(), true)
+            assertEquals(emulateNFA(File("src/test/kotlin/resources/badDfa2").inputStream()) ,emulateNFA(File("testMin7").inputStream()))
+        }
+
+
     }
 
 
